e16cdcf9a48e21cc84861d772528fa207e630973
bluecoder01 <onesixteenj@gmail.com>
2024-06-10 15:09:11 +0100

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
        fontFamily: {
         body: ['Roboto', 'Arial', 'sans-serif'],
         title: ['Roboto', 'Arial', 'sans-serif']
+      },
+      colors: {
+        "primary": "#002626",
+        "secondary": "#538c61",
+        "alt": "#94b8e4",
       }
     },
   },

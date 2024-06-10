ce35226a274b50240653a0f3b5f1d9f45ba57703
bluecoder01 <onesixteenj@gmail.com>
2024-06-10 15:16:00 +0100

    nil

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,31 +1,31 @@
 // import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import Contact from './pages/contact/Contact';
-import Dashboard from './pages/dashboard/Dashboard';
-import Product from './pages/product/Product';
-import Features from './pages/features/Features';
-import Pricing from './pages/pricing/Pricing';
-import Login from './pages/auth/Login';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Product from "./pages/product/Product";
+import Features from "./pages/features/Features";
+import Pricing from "./pages/pricing/Pricing";
+import Login from "./pages/auth/Login";
 
 const Layout = () => {
   return (
-    <div className='layout'>
+    <div className="layout">
       <>
-          <Routes>
-            <Route element={<Home />} path='/'/>
-            <Route element={<About />} path='/about-us'/>
-            <Route element={<Contact />} path='/contact'/>
-            <Route element={<Dashboard />} path='/dashboard'/>
-            <Route element={<Product />} path='/product'/>
-            <Route element={<Features />} path='/features'/>
-            <Route element={<Pricing />} path='/pricing'/>
-            <Route element={<Login />} path='/login'/>
-          </Routes>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<About />} path="/about-us" />
+          <Route element={<Contact />} path="/contact" />
+          <Route element={<Dashboard />} path="/dashboard" />
+          <Route element={<Product />} path="/product" />
+          <Route element={<Features />} path="/features" />
+          <Route element={<Pricing />} path="/pricing" />
+          <Route element={<Login />} path="/login" />
+        </Routes>
       </>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;

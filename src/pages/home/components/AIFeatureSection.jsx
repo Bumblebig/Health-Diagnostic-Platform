8a54582a475cd63fe0ce491f621ea8cb21556f98
Abdurrahman <dreydare16@gmail.com>
2024-06-12 13:22:55 +0100

    adjusted ai section responsiveness

--- src/pages/home/components/AIFeatureSection.jsx
+++ src/pages/home/components/AIFeatureSection.jsx
@@ -4,13 +4,13 @@ import AIIllustration from "../assets/ai-illustration.json";
 
 export default function AIFeatureSection() {
     return (
-        <section className="bg-secondary py-14 px-4 text-white">
-            <h2 className="text-center font-semibold text-lg">Introducing Our Medical AI Assistant</h2>
-            <p className="text-center text-sm mt-5">Our cutting-edge Medical AI Assistant is designed to enhance your health monitoring experience by providing personalized medical insights and recommendations. By analyzing your symptoms and health data, the AI can diagnose potential conditions and suggest possible treatments. This intelligent assistant leverages the latest advancements in artificial intelligence to offer accurate and reliable health advice right at your fingertips.</p>
+        <section className="bg-secondary py-14 px-4 text-white lg:px-12">
+            <h2 className="text-center font-semibold text-lg lg:text-xl">Introducing Our Medical AI Assistant</h2>
+            <p className="text-center text-sm mt-5 lg:text-lg lg:px-5">Our cutting-edge Medical AI Assistant is designed to enhance your health monitoring experience by providing personalized medical insights and recommendations. By analyzing your symptoms and health data, the AI can diagnose potential conditions and suggest possible treatments. This intelligent assistant leverages the latest advancements in artificial intelligence to offer accurate and reliable health advice right at your fingertips.</p>
 
-            <div className="mt-20">
-                <article className="flex flex-col gap-10">
-                    <div className="flex flex-col gap-10 text-center">
+            <div className="mt-20 lg:flex lg:w-full lg:justify-between lg:items-center lg:mt-10 lg:px-5">
+                <article className="flex flex-col gap-10 md:gap-14 lg:w-1/2">
+                    <div className="flex flex-col gap-10 text-center md:flex-row md:items-start md:text-left md:justify-between">
                         <Benefits
                             title="Symptom Diagnosis"
                             desc="Simply input your symptoms, and our AI will analyze them to provide a list of possible conditions. This helps you understand what might be affecting your health and guides you on the next steps to take."
@@ -20,7 +20,7 @@ export default function AIFeatureSection() {
                             desc="Based on the diagnosed conditions, our AI offers evidence-based treatment recommendations, including lifestyle changes, over-the-counter medications, and when to seek professional medical advice"
                         />
                     </div>
-                    <div className="flex flex-col gap-10 text-center">
+                    <div className="flex flex-col gap-10 text-center md:flex-row md:items-start md:text-left md:justify-between">
                         <Benefits
                             title="Personalized Health Insights"
                             desc="Receive tailored health tips and insights based on your unique health data, ensuring that you get the most relevant advice for maintaining your well-being"
@@ -32,8 +32,8 @@ export default function AIFeatureSection() {
                     </div>
 
                 </article>
-                <div className="mt-10">
-                    <Lottie animationData={AIIllustration} loop={true} autoplay={true} className="w-full" />
+                <div className="mt-10 lg:w-2/5">
+                    <Lottie animationData={AIIllustration} loop={true} autoplay={true} className="w-full block md:w-4/5 md:mx-auto md:mt-4 lg:w-full" />
                 </div>
             </div>
         </section>

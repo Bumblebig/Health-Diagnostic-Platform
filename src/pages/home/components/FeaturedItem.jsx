e14add506911bb333e76f6ee1da4d3844b4b007b
Abdurrahman <dreydare16@gmail.com>
2024-06-12 01:03:35 +0100

    included features section

---
+++ src/pages/home/components/FeaturedItem.jsx
@@ -0,0 +1,8 @@
+export default function FeaturedItem({ headline, desc }) {
+    return (
+        <div className="mb-5 text-center lg:text-left lg:pt-12 lg:pr-8">
+            <h2 className="text-xl font-semibold mb-3 leading-snug lg:text-2xl">{headline}</h2>
+            <p className="lg:text-lg">{desc}</p>
+        </div>
+    )
+}

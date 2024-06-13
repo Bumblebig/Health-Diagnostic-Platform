023921605c72cbd4acbd947fabaec5fbd6513984
Abdurrahman <dreydare16@gmail.com>
2024-06-13 14:11:29 +0100

    included the call to action section

---
+++ src/pages/home/components/CTASection.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function CTA() {
+    return (
+        <section className="w-full h-auto relative py-16 bg-[#00000061]">
+            <video autoPlay muted loop playsInline className="w-full object-cover absolute top-0 left-0 h-full -z-10">
+                <source src="https://videos.pexels.com/video-files/6130551/6130551-hd_1280_720_30fps.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            <div className="w-4/5 bg-white mx-auto py-8 px-8">
+                <h2 className="text-center text-primary font-semibold text-lg">Ready to Take Control of Your Health?</h2>
+
+                <p className="text-sm mt-5 leading-normal">Join thousands of others who are monitoring their health in real-time with our Health Diagnostic Device.
+                    Stay informed, stay healthy, and share your data with those who matter.</p>
+
+                <Link to="/login" className="text-sm inline-block mt-8 bg-primary text-white px-3 py-2 rounded hover:bg-[#002626dd]">Get Started Today</Link>
+            </div>
+        </section>
+    )
+}
--- src/pages/home/components/index.js
+++ src/pages/home/components/index.js
@@ -3,3 +3,4 @@ export { default as Features } from "./Features";
 export { default as FeaturedItem } from "./FeaturedItem";
 export { default as AISection } from "./AIFeatureSection";
 export { default as Benefits } from "./AIBenefits";
+export { default as CTA } from "./CTASection"
